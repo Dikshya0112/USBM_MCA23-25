32ede29498f08eaa73740564580780f2fa02f4d4
Sudikshya Patra <sudikshyapatra0112@gmail.com>
2024-10-07 18:25:14 +0530

    class2

---
+++ FrontEnd/JavaScript/class2/data.js
@@ -0,0 +1,56 @@
+
+//undefind
+
+let data;
+console.log(typeof data);
+console.log(data);
+
+//number
+let age=22;
+console.log("My age is : ",age);
+console.log(typeof age);
+let pi=3.14;
+console.log(pi);
+console.log(typeof pi);
+//let num=08; error because decimal with leading zero are not allowed here
+
+//Addition
+let num1=10;
+let num2=40;
+let addition=num1+num2;
+console.log(addition)
+//sub
+num1=10;
+ num2=4;
+let sub=num1-num2;
+console.log(sub);
+
+//mul
+ num1=10;
+num2=10;
+ let mul=num1*num2;
+console.log(mul);
+
+//div
+ num1=11;
+num2=4;
+let div=num1/num2;
+console.log(div);
+
+// boolean
+     
+
+
+
+//string
+// let ch='sudikshya';
+// let name="Sudikshya"
+// console.log(ch,name)
+
+let name=" Sudikshya ";
+let Age =22;
+let stream="MCA";
+let rollNo=139;
+let city="Khordha";
+let clgName="United School Of Business Management";
+console.log(`My name is ${name} , age : ${Age} , stream : ${stream},city : ${city},college : ${clgName} ,roll no : ${rollNo}`)
