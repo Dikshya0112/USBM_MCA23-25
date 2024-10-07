c2d0468785bd06eb626ca3a7f531ecc3e6f974af
Sudikshya Patra <sudikshyapatra0112@gmail.com>
2024-10-07 22:00:24 +0530

    task added

---
+++ FrontEnd/JavaScript/class2/task.js
@@ -0,0 +1,26 @@
+// question 1
+
+let foodPrice=25;
+let drinkPrice=10;
+let total = foodPrice + drinkPrice ;
+console.log("You need to give a 10% tip as per the bill");
+console.log('Your total is ', total);//35
+let tip= total*(10/100)
+console.log('after tip the total is' ,total + tip );//38.5
+
+
+// question 2
+let originalPrice=3000;
+let discount=20;
+let finalPrice=  originalPrice - discount
+console.log('final price is', finalPrice);//2980
+
+//question 3
+
+let productPrice=2000
+let salexTaxRate=10
+let salesTax= (productPrice*salexTaxRate)/100
+console.log('sales tax is', salesTax)
+let totalPrice = productPrice + salesTax
+console.log('total price after adding sales tax :', totalPrice)
+
