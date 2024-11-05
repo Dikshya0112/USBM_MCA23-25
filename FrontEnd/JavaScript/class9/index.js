c95518df2b9b5f447e1da4be82c25829cd2aa932
Sudikshya Patra <sudikshyapatra0112@gmail.com>
2024-11-05 09:52:09 +0530

    class 9

---
+++ FrontEnd/JavaScript/class9/index.js
@@ -0,0 +1,51 @@
+/*hof- higher order funt
+map()
+filter()
+reduce()
+
+
+RETURN:
+return the value
+stops the execution
+  */
+//    let arr= ['a','b','c','d']
+//   let b=  arr.forEach((ele,i) => {
+//     console.log(ele,i);// a,b,c,d
+    
+//    });
+//    console.log(b);//undefined
+   
+
+
+//MAP:
+
+//   let a=  arr.map((ele,i)=>{
+// // console.log(ele);//a b c d
+// console.log(ele.toUpperCase());
+
+//    })
+//    console.log(a);//[undefined,undefined,undefined,undefined]
+   
+// let a=  arr.map((ele,i)=>{
+//     // console.log(ele);//a b c d
+//     return(ele.toUpperCase());
+    
+//        })
+//        console.log(a);//['A','B','C','D']
+       
+
+// FILTER:
+
+// let arr2=[1,2,3,4,5,6]
+// const odd = arr2.filter((ele)=>{
+// return ele%2!==0
+// })
+// console.log(odd);//[1,3,5]
+
+//REDUCE
+
+// const reduce= arr2.reduce((acc,num)=> acc+num , 0
+// )
+// console.log(reduce);//21
+
+
---
+++ FrontEnd/JavaScript/class9/object.js
@@ -0,0 +1,110 @@
+//object : key ,value -> pair
+// key can be any datatype
+// let student1 = { // literal
+//     'name':'Sudikshya',
+//     'age':21,
+//     'stream':'MCA',
+// }
+// console.log(typeof student1);// object
+// // obj is meaningfull as compared to array becoz of its key
+
+// //using constructor
+// const stud2= new Object()
+// stud2.name = "Abhi"
+// stud2.age= 22
+// console.log(stud2);
+
+//todo :
+// to create arr objs of 50 students
+// let student1 = [
+//   {
+//     // literal
+//     name: "Sudikshya",
+//     age: 21,
+//     stream: "MCA",
+//   },
+//   {
+//     name: "Ankita",
+//     age: 21,
+//     stream: "MCA",
+//   },
+//   {
+//     name: "Sonali",
+//     age: 21,
+//     stream: "MCA",
+//   },
+//   {
+//     name: "Madhu",
+//     age: 22,
+//     stream: "MCA",
+//   },
+// ];
+//access
+// dot notation
+// bracket Notification
+
+// // modify
+// student1.name="Abhiii"
+
+//nested obj
+// let person = {
+//   name: "abc",
+//   age: 20,
+//   address: { landmark: "infocity", pin: 751024 },
+// };
+// console.log(person);
+
+
+//commentss obj
+// let user = {
+//     username:'abc',
+//     email:'abc@gmail.com',
+//     gender:'female',
+//     age:30,
+//     comments:['hh','httyh','thynh'],
+//     address:{
+//         city:'bbsr',
+//         pin:752024,
+//         state:'odisha'
+//     }
+// }
+// console.log(user.username,
+// user.email,
+// user.address.city,
+// user.address.state,
+// user.comments,
+// user.comments[0],
+// user.comments[user.comments.length - 1])
+
+//this keyword - it refers toyhe current obj
+// let stud = {
+//     name:'Abhishek',
+//     age:22,
+//     greet:function(){
+//         console.log(`hello ${this.name}`);   
+//     }
+// }
+// // console.log(stud.greet());
+// stud.greet()
+
+
+let product ={
+    id:1,
+    pro_Name: 'phone',
+    stock :30, //60
+    restock(n){
+        this.stock += n
+    }
+}
+product.restock(30)
+console.log(
+    product
+);
+
+//IIFE= immediately invoked function expression
+//API= Application Programming Interface
+// server -  appn (connection establish - API) 
+// json -local storage (save) = stringify
+// json.stringify()
+// Object -> JSON
+// JSON => Object   -   JSON.parse
