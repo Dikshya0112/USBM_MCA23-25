dd635aedbf5a95cdd4a4b5e90bec9c2f609ddf3d
Sudikshya Patra <141931035+Dikshya0112@users.noreply.github.com>
2024-12-12 23:29:10 +0530

    Delete FrontEnd/Project/Mini Project 2 directory

--- FrontEnd/Project/Mini Project 2/e-commerce/src/components/ProtectedRoute.jsx
+++
@@ -1,9 +0,0 @@
-import { useAuth  } from "./AuthProvider";
-import{Navigate ,useNavigate} from 'react-router-dom'
-function ProtectedRoute({ children }) {
-    const { isAuthenticated } = useAuth();
-    const isAuth = isAuthenticated || JSON.parse(localStorage.getItem('isAuthenticated'));
-  
-    return isAuth ? children : <Navigate to="/login" replace />;
-  }
-export default ProtectedRoute
